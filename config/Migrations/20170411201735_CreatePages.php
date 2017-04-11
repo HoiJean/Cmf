589037c97e7c5e4bb01542acf3d01314d7db4ad2
HoiJean <jean.mail92@gmail.com>
2017-04-11 22:27:08 +0200

    Init Crud pages

---
+++ config/Migrations/20170411201735_CreatePages.php
@@ -0,0 +1,44 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreatePages extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('pages');
+        $table->addColumn('title', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('slug', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('content', 'text', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('data', 'text', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
